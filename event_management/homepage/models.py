80fa799eefe18f6e184e19535804658fd476dfdd
pl366 <pl366@snu.edu.in>
2017-10-09 01:57:57 +0530

    Integrating tag.html page

--- event_management/homepage/models.py
+++ event_management/homepage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Reg_User(models.Model):
+    Username = models.CharField(max_length=32)
+    Email = models.EmailField(max_length=32)
+    Password= models.CharField(max_length=32)
+ 
+
--- event_management/homepage/urls.py
+++ event_management/homepage/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+"""from django.conf.urls import url
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
@@ -7,5 +7,20 @@ urlpatterns = [
     url(r'^register/', views.register,name='register'),
     url(r'^login/$', auth_views.login,{'template_name':'homepage/login.html'}),
     url(r'^logout/$', auth_views.logout,{'template_name':'homepage/logout.html'}),
+    url(r'^register/tag/$', views.tag,name='tag'),
     
+ ]
+"""
+from django.conf.urls import url
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^clubs/$', views.club, name='club'),
+    url(r'^register/', views.register,name='register'),
+    # url(r'^login/$', auth_views.login,{'template_name':'homepage/LoginPage.html'}),
+    url(r'^logout/$', auth_views.logout,{'template_name':'homepage/index.html'}),
+    url(r'^login/', views.Login, name='Login'),
+    url(r'^tag/$', views.tag,name='tag'),
+    url(r'^sel_tag/$', views.sel_tag,name='sel_tag'),
  ]
